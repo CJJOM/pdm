679dcf52065d73fbe3f439b70d1b02cf8c68d63f
Jomar <djjomar28@gmail.com>
2020-05-18 19:07:42 -0400

    project

--- AwesomeProject/src/login/constants.js
+++
@@ -1,6 +0,0 @@
-export default {
-    PASS: 'password',
-    USER: 'Username',
-    DEFAULT_MARGIN_BUTTON: 50,
-    TITLE_BUTTON: 'INGRESAR'
-}
--- DefensaHito3/src/Config/Images.js
+++
@@ -1,9 +0,0 @@
-export default {
-    LOGO: require('../../assets/logo.png'),
-    PASSWORD: require('../../assets/pass.png'),
-    USERNAME: require('../../assets/username.png'),
-    EMAIL: require('../../assets/email1.png'),
-    FPDM: require('../../assets/img1.png'),
-    FDEFH3: require('../../assets/img2.png'),
-    FFIRE: require('../../assets/img3.png')
-}
